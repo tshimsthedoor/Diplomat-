e62d1592bbbf8d70b5812a9d6dd4e5df751fed58
Tresor Tshimanga Kazadi <tshims79@gmal.com>
2022-05-17 19:47:54 +0200

    Add project files.

---
+++ BookStore/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objectCategory = _db.Categories;
+            return View(objectCategory);
+        }
+        
+        //GET
+        public IActionResult Create()
+        {
+            
+            return View();
+        } 
+        
+        //POST
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+    }
+}
